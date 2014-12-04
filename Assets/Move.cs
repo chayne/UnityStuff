b37fda7af2756c5a8e2d63cf9e9a9950f5957007
Simon <executor7@gmx.net>
2014-12-04 16:35:36 +0100

    init

---
+++ Assets/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		//transform.position += Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position += Vector3.right*Time.deltaTime;
+	}
+}
